478e8641186b60ec60c583f780697fc2469fdfc7
praveensingh1999 <praveen969500@gmail.com>
2025-11-06 01:36:40 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserInfoForm from './UserInfoForm';
+
+function App() {
+  return (
+    <div>
+      <UserInfoForm />
+    </div>
+  );
+}
+
+export default App;
